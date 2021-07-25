6869e504972730ff5cf248ae0671d866ac90fdf7
Oliver <oliveroliverio@gmail.com>
2021-07-25 14:28:58 -0700

    Added a new list to follow along with exercise, and made the first item bold

--- src/Main2.js
+++ src/Main2.js
@@ -4,7 +4,17 @@ function Main2() {
         <article>
             <h2>Converse Chuck Taylor All Star Low Top</h2>
             <img src="https://i.ibb.co/Jr7Wh1d/challenges.jpg" alt="Chuck Taylor All Star Shoe" height="250" width="250" />
-            
+            <hr></hr>
+
+            <h1>I'm just going to add this for the sake of exercise</h1> 
+            <ul className="class1">
+                <li>Some stuff</li>
+                <li>Some more stuff</li>
+                <li>And some things</li>
+                <li>More things</li>
+                <li>whatrever</li>
+            </ul>
+            <hr></hr>
             <p><strong>$65.00</strong></p>
             <p>Free shipping</p>
             <p>Ready to dress up or down, these classic canvas Chucks are an everyday wardrobe staple.</p>
