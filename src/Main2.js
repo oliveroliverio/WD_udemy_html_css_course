61fbead220a416680fd09412445c7e2d5e335d78
Oliver <oliveroliverio@gmail.com>
2021-07-25 14:50:27 -0700

    wrapped everything around a body tag, added a header tag titled 'The Shoe Magazine' with 4 links

--- src/Main2.js
+++ src/Main2.js
@@ -1,40 +1,51 @@
 import "./Main2.css"
 function Main2() {
     return (
-        <article>
-            <h2>Converse Chuck Taylor All Star Low Top</h2>
-            <img src="https://i.ibb.co/Jr7Wh1d/challenges.jpg" alt="Chuck Taylor All Star Shoe" height="250" width="250" />
-            <hr></hr>
+        <body>
+            <header className="main-header"></header>
+                <h1>The Shoe Magazine</h1>
+                    <nav>
+                        <a href="asdf.com">Vans</a>
+                        <a href="asdf.com">DC</a>
+                        <a href="asdf.com">Converse</a>
+                        <a href="asdf.com">Reebok</a>
+                    </nav>
+        
+            <article>
+                <h2>Converse Chuck Taylor All Star Low Top</h2>
+                <img src="https://i.ibb.co/Jr7Wh1d/challenges.jpg" alt="Chuck Taylor All Star Shoe" height="250" width="250" />
+                <hr></hr>
 
-            <h1>I'm just going to add this for the sake of exercise</h1> 
-            <ul>
-                <li>Some stuff</li>
-                <li>Some more stuff</li>
-                <li>And some things</li>
-                <li>More things</li>
-                <li>whatrever</li>
-            </ul>
+                <h1>I'm just going to add this for the sake of exercise</h1> 
+                <ul>
+                    <li>Some stuff</li>
+                    <li>Some more stuff</li>
+                    <li>And some things</li>
+                    <li>More things</li>
+                    <li>whatrever</li>
+                </ul>
 
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, sit unde quia ea maiores ab magnam nihil earum laborum consequuntur perferendis dolorum placeat commodi id? Voluptates dicta commodi necessitatibus temporibus?</p>
-            
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsa, corrupti iste explicabo tempora iusto illum et blanditiis nostrum maxime, atque sit incidunt nihil quam doloribus aliquid ad cum harum.</p>
-            <hr></hr>
-            <p><strong>$65.00</strong></p>
-            <p>Free shipping</p>
-            <p>Ready to dress up or down, these classic canvas Chucks are an everyday wardrobe staple.</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, sit unde quia ea maiores ab magnam nihil earum laborum consequuntur perferendis dolorum placeat commodi id? Voluptates dicta commodi necessitatibus temporibus?</p>
+                
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsa, corrupti iste explicabo tempora iusto illum et blanditiis nostrum maxime, atque sit incidunt nihil quam doloribus aliquid ad cum harum.</p>
+                <hr></hr>
+                <p><strong>$65.00</strong></p>
+                <p>Free shipping</p>
+                <p>Ready to dress up or down, these classic canvas Chucks are an everyday wardrobe staple.</p>
 
-            <a href="https://www.converse.com">More information &rarr;</a>
-            
-            <h3>Product details</h3>
-            <ul>
-                <li>Lightweight, durable canvas sneaker</li>
-                <li>Lightly padded footbed for added comfort</li>
-                <li>Iconic Chuck Taylor ankle patch</li>
-            </ul>
-            
-            <button>Add to cart</button>
-  
-</article>
+                <a href="https://www.converse.com">More information &rarr;</a>
+                
+                <h3>Product details</h3>
+                <ul>
+                    <li>Lightweight, durable canvas sneaker</li>
+                    <li>Lightly padded footbed for added comfort</li>
+                    <li>Iconic Chuck Taylor ankle patch</li>
+                </ul>
+                
+                <button>Add to cart</button>
+    
+            </article>
+        </body>
     )
 }
 
